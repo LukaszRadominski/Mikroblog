c7953488c657bcaaa953301064f8d811a8a5b6b2
Łukasz Radomiński <lukaszr82@o2.pl>
2022-09-05 14:53:18 +0200

    Update config and init

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,15 @@
 
 
 from flask import Flask    # 1
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)      # 2 Nazwa __name__ to predefiniowana w Pythonie zmienna, której wartością może być "__main__", gdy moduł wykonywany jest bezpośrednio. Jeśli moduł uruchamiany jest jako zależność, to w __name__ będzie znajdowała się jego nazwa.
+app.config.from_object(Config) # Dodaję tu odczyt konfiguracji z obiektu Config
+db = SQLAlchemy(app)  # wprowadzimy instancję bazy danych 
+migrate = Migrate(app, db) # dodaję migrację
 
-from app import routes     # 3  tworzymy tu po prostu instancję aplikacji i importujemy moduł routes.py. #  import pochodzi  paczki app, czyli czegoś zdefiniowanego przez nazwę folderu app oraz pliku __init__.py. Zmienna app, będąca instancją Flask, jest zawarta w tej paczce. Za chwilę to zobaczymy. Import na dole to z kolei pewna sztuczka, która pomoże nam zabezpieczyć się przed problemem cyklicznych importów.
+
+from app import routes     # 3  Import dopiero w tym wierszu to z kolei pewna sztuczka, która pomoże nam zabezpieczyć się przed problemem cyklicznych importów.  tworzymy tu po prostu instancję aplikacji i importujemy moduł routes.py. #  import pochodzi  paczki app, czyli czegoś zdefiniowanego przez nazwę folderu app oraz pliku __init__.py. Zmienna app, będąca instancją Flask, jest zawarta w tej paczce. Za chwilę to zobaczymy.
